502a1adeec2b5ffd86c9a578e0cb1075d7d122cb
Wahtu Astrawan <chatt699@gmail.com>
2025-04-14 23:35:35 +0800

    style: adding leaflet css

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,6 +2,7 @@ import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import 'leaflet/dist/leaflet.css';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
